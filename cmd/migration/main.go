d62bfd4af76dcae79628c18959719de4ae6f4c31
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-20 15:09:10 -0300

    Move migration to helper folder to use on tests

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/OvictorVieira/transact.ease/cmd/helpers"
 	"github.com/OvictorVieira/transact.ease/internal/config"
 	"github.com/OvictorVieira/transact.ease/internal/constants"
+	"github.com/OvictorVieira/transact.ease/pkg/helpers"
 	"github.com/OvictorVieira/transact.ease/pkg/logger"
-	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
-	"os"
-	"path/filepath"
-	"sort"
-)
-
-const (
-	dir = "cmd/migration/migrations"
 )
 
 var (
@@ -42,52 +33,16 @@ func main() {
 	defer db.Close()
 
 	if up {
-		err = migrate(db, "up")
+		err = helpers.Migrate(db, "up")
 		if err != nil {
 			logger.Fatal(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryMigration})
 		}
 	}
 
 	if down {
-		err = migrate(db, "down")
+		err = helpers.Migrate(db, "down")
 		if err != nil {
 			logger.Fatal(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryMigration})
 		}
 	}
-}
-
-func migrate(db *sqlx.DB, action string) (err error) {
-	logger.InfoF("running migration [%s]", logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryMigration}, action)
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	files, err := filepath.Glob(filepath.Join(cwd, dir, fmt.Sprintf("*.%s.sql", action)))
-	if err != nil {
-		return constants.ErrWhenGetFiles
-	}
-
-	sort.Slice(files, func(i, j int) bool {
-		return helpers.ExtractLeadingNumber(files[i]) < helpers.ExtractLeadingNumber(files[j])
-	})
-
-	for _, file := range files {
-		logger.Info("Executing migration", logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryMigration, constants.LoggerFile: file})
-		data, err := os.ReadFile(file)
-		if err != nil {
-			return constants.ErrWhenReadFiles
-		}
-
-		_, err = db.Exec(string(data))
-		if err != nil {
-			fmt.Println(err)
-			return fmt.Errorf("error when exec query in file: %v", file)
-		}
-	}
-
-	logger.InfoF("migration [%s] success", logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryMigration}, action)
-
-	return
 }
