e6f9da2d7c23bf37c2d260cc6a7da020db8c3d4e
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-23 10:58:38 -0300

    Added Begin method to Database mock

--- internal/repositories/database_interface.go
+++ internal/repositories/database_interface.go
@@ -2,12 +2,14 @@ package repositories
 
 import (
 	"context"
+	"database/sql"
 	"github.com/jmoiron/sqlx"
 )
 
 type Database interface {
 	NamedQueryContext(ctx context.Context, query string, arg interface{}) (*sqlx.Rows, error)
 	GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+	Begin() (*sql.Tx, error)
 }
 
 type SqlxDatabase struct {
@@ -20,4 +22,8 @@ func (s SqlxDatabase) NamedQueryContext(ctx context.Context, query string, arg i
 
 func (s SqlxDatabase) GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
 	return s.DB.GetContext(ctx, dest, query, args...)
+}
+
+func (s SqlxDatabase) Begin() (*sql.Tx, error) {
+	return s.DB.Begin()
 }
