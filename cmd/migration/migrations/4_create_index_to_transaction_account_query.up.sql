4e8b073c661d54c0650daf1a10f1235fb1353ac5
Victor Vieira <victor.h.souza.vieira@gmail.com>
2023-10-26 16:40:43 -0300

    Fix migration adding IF NOT EXISTS

--- cmd/migration/migrations/4_create_index_to_transaction_account_query.up.sql
+++ cmd/migration/migrations/4_create_index_to_transaction_account_query.up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX idx_transactions_account_operation ON transact_ease.transactions (account_id, operation_type_id);
+CREATE INDEX IF NOT EXISTS idx_transactions_account_operation ON transact_ease.transactions (account_id, operation_type_id);
